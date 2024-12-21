f8a8ca94c9268a42f6526a40e05b6390f08a0dcd
rkorzen <korzeniewski@gmail.com>
2024-12-21 13:32:06 +0100

    cwiczene_03

--- cwiczenia/cwiczenie_03.py
+++ cwiczenia/cwiczenie_03.py
@@ -14,3 +14,15 @@ assert wybierz(lista, lambda x: x > 2, lambda x: x == 8) == [3, 4, 5, 6, 7], "wy
 assert wybierz(lista2, lambda x: x > 2, lambda x: x % 2 == 0) == [3, 5, 7, 9, 11, 13, 15, 17, 19], "wynik niezgodny z oczekiwanym"
 
 """
+
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+lista2 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 22, 23, 7]
+
+def wybierz(lista, start, stop):
+    pass
+
+assert wybierz(lista, lambda x: x > 2, lambda x: x == 8) == [3, 4, 5, 6, 7], "wynik niezgodny z oczekiwanym"
+
+assert wybierz(lista2, lambda x: x > 2, lambda x: x % 2 == 0) == [3, 5, 7, 9, 11, 13, 15, 17, 19], "wynik niezgodny z oczekiwanym"
