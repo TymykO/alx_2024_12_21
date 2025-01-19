9f6c05105f97f2e882b7bb9f0fafd4bbf0e8ca99
rkorzen <korzeniewski@gmail.com>
2025-01-19 10:17:00 +0100

    logowanie wylogowanie

--- projekty/blog/blog/settings.py
+++ projekty/blog/blog/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = "posts:login"
-LOGIN_REDIRECT_URL = "/"
-# LOGOUT_REDIRECT_URL = "/login/"
+LOGIN_REDIRECT_URL = "posts:list"
+LOGOUT_REDIRECT_URL = "posts:login"
--- projekty/blog/posts/urls.py
+++ projekty/blog/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth.views import LogoutView
 app_name = "posts"
 urlpatterns = [
     path("", views.posts_list, name="list"),
     path("login/", views.login_view, name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("<int:post_id>/", views.post_details, name="details"),
 ]
