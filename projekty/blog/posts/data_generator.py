664137deb3b4ce0f6b3d9c3efc98fef1b81f8c68
rkorzen <korzeniewski@gmail.com>
2025-01-19 13:33:52 +0100

    paginacja - wstep

--- projekty/blog/posts/data_generator.py
+++ projekty/blog/posts/data_generator.py
@@ -18,3 +18,28 @@ generate_posts(100)
 
 
 """
+from faker import Faker
+
+from posts.models import Post
+from django.contrib.auth.models import User
+
+
+
+def generate_posts(n, language="pl_PL"):
+    faker = Faker(language)
+    authors = User.objects.all()
+    statuses = ["published", "draft"]
+    for _ in range(n):
+        title = faker.sentence()
+
+        content_length = faker.random_int(100, 2000)
+        content = faker.text(content_length)
+
+        author = faker.random_element(authors)
+        status = faker.random_element(statuses)
+
+        post = Post(title=title, content=content, author=author, status=status)
+        post.save()
+
+    print(f"Generated {n} posts")
+
