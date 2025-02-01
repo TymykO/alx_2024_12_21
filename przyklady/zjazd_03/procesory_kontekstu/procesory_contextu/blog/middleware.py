c503d87cfdabb367627e8f54696aa9d4260e8417
rkorzen <korzeniewski@gmail.com>
2025-02-01 13:54:41 +0100

    middleware

--- przyklady/zjazd_03/procesory_kontekstu/procesory_contextu/blog/middleware.py
+++ przyklady/zjazd_03/procesory_kontekstu/procesory_contextu/blog/middleware.py
@@ -1,11 +1,25 @@
+import logging
+
 import time
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
 class TimeitMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         start_time = time.time()
+
         response = self.get_response(request)
+        
         end_time = time.time()
-        request.headers["X-Time"] = f"{end_time - start_time} seconds"
+        response["X-Time"] = f"{end_time - start_time} seconds"
+        logger.info(f"Time taken: {end_time - start_time} seconds")
         return response
+
+
+# logować request.method i request.path
+# logowa response.status_code
