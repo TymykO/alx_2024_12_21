6f9f00dec096357f3a9a0e3ce97e2d66aa8625f0
rkorzen <korzeniewski@gmail.com>
2025-02-01 11:25:31 +0100

    widok generyczny CreateView

--- projekty/widoki_klasowe/posts/urls.py
+++ projekty/widoki_klasowe/posts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("",views.HomeView.as_view(), name="index"),
     path("posts/", views.PostListView.as_view(), name="list"),
     path("posts/<int:pk>/", views.PostDetailView.as_view(), name="detail"),
+    path("posts/new/", views.PostCreateView.as_view(), name="create"),
 ]
--- projekty/widoki_klasowe/posts/views.py
+++ projekty/widoki_klasowe/posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView, DetailView, TemplateView, CreateView
 from .models import Post
-
+from django.urls import reverse_lazy
 # Create your views here.
 
 class HomeView(TemplateView):
@@ -14,3 +14,8 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ["title", "content"]
+    # success_url = reverse_lazy("posts:list")  # to zastępuje redirect po zapisie, który domyślnie bieże się z get_absolute_url modelu
